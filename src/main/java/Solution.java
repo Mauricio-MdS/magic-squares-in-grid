78453188fbb888999708247043313018a4e30cc8
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-08-09 17:18:25 -0300

    Refactor to use magical properties and optimize execution.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -6,30 +5,35 @@ class Solution {
     public int numMagicSquaresInside(int[][] grid) {
         int squares = 0;
         for (int rowIndex = 0; rowIndex < grid.length - 2; rowIndex++) {
-            for (int colIndex = 0; colIndex < grid[0].length -2; colIndex++) {
-                if (magicSquare(grid, rowIndex, colIndex)) squares++;
+            for (int colIndex = 0; colIndex < grid[0].length - 2; colIndex++) {
+                if (isMagic(grid, rowIndex, colIndex)) squares++;
             }
         }
         return squares;
     }
 
-    private boolean magicSquare(int[][] grid, int rowIndex, int colIndex) {
+    private boolean isMagic(int[][] grid, int rowIndex, int colIndex) {
+        if (grid[rowIndex + 1][colIndex + 1] != 5) return false;
+
+        int[] nums = new int[9];
+        int index = 0;
         Set<Integer> set = new HashSet<>();
-        int[] col = new int[3];
+
         for (int i = rowIndex; i < rowIndex + 3; i++) {
-            int row = 0;
             for (int j = colIndex; j < colIndex + 3; j++) {
                 int x = grid[i][j];
                 if (x < 1 || x > 9 || !set.add(x)) return false;
-                row += x;
-                col[j - colIndex] += x;
+                nums[index++] = x;
             }
-            if (row != 15) return false;
         }
 
-        if (grid[rowIndex][colIndex] + grid[rowIndex + 1][colIndex + 1] + grid[rowIndex + 2][colIndex + 2] != 15 ||
-                grid[rowIndex][colIndex + 2] + grid[rowIndex + 1][colIndex + 1] + grid[rowIndex + 2][colIndex] != 15)
-            return false;
-        return Arrays.stream(col).allMatch(value -> value == 15);
+        return nums[0] + nums[1] + nums[2] == 15 &&
+                nums[3] + nums[4] + nums[5] == 15 &&
+                nums[6] + nums[7] + nums[8] == 15 &&
+                nums[0] + nums[3] + nums[6] == 15 &&
+                nums[1] + nums[4] + nums[7] == 15 &&
+                nums[2] + nums[5] + nums[8] == 15 &&
+                nums[0] + nums[4] + nums[8] == 15 &&
+                nums[2] + nums[4] + nums[6] == 15;
     }
 }
